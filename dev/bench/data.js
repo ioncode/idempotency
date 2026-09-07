71ce33f48a41552a6af4adb55dcc15879f9776a8
github-action-benchmark <github@users.noreply.github.com>
2026-09-07 12:47:37 +0000

    add Benchmark (go) benchmark result for e715870f6702175283366bd3b486186912763ba1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788784466012,
+  "lastUpdate": 1788785257585,
   "repoUrl": "https://github.com/ioncode/idempotency",
   "entries": {
     "Benchmark": [
@@ -44,6 +44,54 @@ window.BENCHMARK_DATA = {
             "value": 35,
             "unit": "allocs/op",
             "extra": "256042 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ionrussia@gmail.com",
+            "name": "Andrey Ponteleev",
+            "username": "ioncode"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e715870f6702175283366bd3b486186912763ba1",
+          "message": "ready for highload & network storms idempotency middleware \n\ntested rc for CI check",
+          "timestamp": "2026-09-07T15:36:48+03:00",
+          "tree_id": "2bd43065c07ce664baa829f5819a62a09e582c35",
+          "url": "https://github.com/ioncode/idempotency/commit/e715870f6702175283366bd3b486186912763ba1"
+        },
+        "date": 1788785256920,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkMiddleware_Processing",
+            "value": 4300,
+            "unit": "ns/op\t    8055 B/op\t      35 allocs/op",
+            "extra": "232842 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMiddleware_Processing - ns/op",
+            "value": 4300,
+            "unit": "ns/op",
+            "extra": "232842 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMiddleware_Processing - B/op",
+            "value": 8055,
+            "unit": "B/op",
+            "extra": "232842 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMiddleware_Processing - allocs/op",
+            "value": 35,
+            "unit": "allocs/op",
+            "extra": "232842 times\n4 procs"
           }
         ]
       }
